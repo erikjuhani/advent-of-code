52205cacc0395b7c9f6b70cd8cefc8081d9fa861
Erik Kinnunen <erik.kinn@gmail.com>
2023-12-02 16:48:54 +0200

    2023: Day 2 - Refactor
    The puzzle was refactored to one solve function that takes in the
    handler function that decides the number to be summed per game.

--- 2023/rust/day2/src/main.rs
+++ 2023/rust/day2/src/main.rs
@@ -1,79 +1,41 @@
-fn part1(input: &str) -> u32 {
-    input
-        .lines()
-        .filter_map(|s| {
-            let g = s.split(':').collect::<Vec<&str>>();
-            return g
-                .first()
-                .cloned()
-                .map(|s| {
-                    s.chars()
-                        .filter_map(|c| c.is_digit(10).then_some(c))
-                        .collect::<String>()
-                })
-                .map(|s| s.parse::<u32>().unwrap())
-                .zip(
-                    g[1..]
-                        .iter()
-                        .map(|s| {
-                            s.split(';')
-                                .map(|s| {
-                                    s.split(',').fold((0, 0, 0), |acc, s| {
-                                        let (amount, color) = s.trim().split_once(' ').unwrap();
-                                        let n = amount.parse::<u32>().unwrap();
-                                        match color {
-                                            "red" => (acc.0 + n, acc.1, acc.2),
-                                            "green" => (acc.0, acc.1 + n, acc.2),
-                                            "blue" => (acc.0, acc.1, acc.2 + n),
-                                            _ => acc,
-                                        }
-                                    })
-                                })
-                                .collect::<Vec<(u32, u32, u32)>>()
-                        })
-                        .flatten()
-                        .all(|(r, g, b)| r <= 12 && g <= 13 && b <= 14)
-                        .then_some(Some(true)),
-                )
-                .map(|(id, _)| id);
-        })
-        .sum::<u32>()
-}
-
-fn part2(input: &str) -> u32 {
+fn solve(input: &str, f: fn((u32, u32, u32, u32)) -> u32) -> u32 {
     input
         .lines()
         .map(|s| {
-            let (_, t) = s.split_once(':').unwrap();
-            return t
-                .split(';')
-                .map(|s| {
-                    s.split(',').fold((0, 0, 0), |acc, s| {
-                        let (amount, color) = s.trim().split_once(' ').unwrap();
-                        let n = amount.parse::<u32>().unwrap();
-                        match color {
-                            "red" => (n, acc.1, acc.2),
-                            "green" => (acc.0, n, acc.2),
-                            "blue" => (acc.0, acc.1, n),
-                            _ => acc,
-                        }
-                    })
-                })
-                .collect::<Vec<_>>();
-        })
-        .map(|x| {
-            x.iter().fold((1, 1, 1), |acc, &(r, g, b)| {
-                (acc.0.max(r), acc.1.max(g), acc.2.max(b))
+            s.split([':', ';', ',']).fold((0, 0, 0, 0), |acc, s| {
+                let n = s
+                    .chars()
+                    .filter_map(|c| c.is_digit(10).then_some(c))
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .unwrap();
+
+                if s.ends_with("red") {
+                    return (acc.0, acc.1.max(n), acc.2, acc.3);
+                } else if s.ends_with("green") {
+                    return (acc.0, acc.1, acc.2.max(n), acc.3);
+                } else if s.ends_with("blue") {
+                    return (acc.0, acc.1, acc.2, acc.3.max(n));
+                } else {
+                    return (n, acc.1, acc.2, acc.3);
+                }
             })
         })
-        .map(|(r, g, b)| r * g * b)
-        .sum::<u32>()
+        .map(f)
+        .sum()
 }
 
 fn main() {
     let input = include_str!("../../../input/day02");
-    println!("answer part1: {}", part1(input));
-    println!("answer part2: {}", part2(input));
+    println!(
+        "answer part1: {}",
+        solve(input, |(id, r, g, b)| if r <= 12 && g <= 13 && b <= 14 {
+            id
+        } else {
+            0
+        })
+    );
+    println!("answer part2: {}", solve(input, |(_, r, g, b)| r * g * b));
 }
 
 #[cfg(test)]
@@ -88,7 +50,18 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 ";
-        assert_eq!(part1(test_input), 8, "for input `{}`", test_input);
+        assert_eq!(
+            solve(test_input, |(id, r, g, b)| {
+                if r <= 12 && g <= 13 && b <= 14 {
+                    id
+                } else {
+                    0
+                }
+            }),
+            8,
+            "for input `{}`",
+            test_input
+        );
     }
 
     #[test]
@@ -100,6 +73,11 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 ";
 
-        assert_eq!(part2(test_input), 2286, "for input `{:#?}`", test_input);
+        assert_eq!(
+            solve(test_input, |(_, r, g, b)| r * g * b),
+            2286,
+            "for input `{:#?}`",
+            test_input
+        );
     }
 }
