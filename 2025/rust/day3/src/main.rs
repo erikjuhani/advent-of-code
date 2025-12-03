a45abae2650b6ff9736f02c0f2178ed90ba6098d
Erik Kinnunen <erik.kinn@gmail.com>
2025-12-03 23:47:53 +0200

    Rust Day 2 - p2
    Didn't get it:(

--- 2025/rust/day3/src/main.rs
+++ 2025/rust/day3/src/main.rs
@@ -1,26 +1,10 @@
+use std::mem::replace;
+
 fn main() {
     println!("DAY1 p1: {}", p1(include_str!("../../../input/day3")));
 }
 
-// One line of digits = One battery bank
-//
-// Turn on exactly 2 batteries
-// Joltage produced = number formed by digits on turned on batteries
-//
-// Bank: 12345, turn on 2 and 4
-// Result: 2 + 4 = 24 jolts
-//
-// Find largest possible joltage each bank can produce
-fn p1(input: &str) -> u32 {
-    // cursor location in bank start at idx 0, increment after loop through chars
-    // ^ skip location
-    // first_char -> loop over the digits
-    // put digits together
-    // make into number
-    // store number in variable
-    // compare if number is bigger
-    // compare first char and window
-
+fn p1(input: &str) -> usize {
     input
         .lines()
         .map(|line| {
@@ -28,7 +12,7 @@ fn p1(input: &str) -> u32 {
 
             for (cursor, a) in line.char_indices() {
                 for b in line[cursor + 1..].chars() {
-                    let n = format!("{a}{b}").parse::<u32>().unwrap();
+                    let n = format!("{a}{b}").parse::<usize>().unwrap();
                     if max_joltage < n {
                         max_joltage = n;
                     }
@@ -40,12 +24,32 @@ fn p1(input: &str) -> u32 {
         .sum()
 }
 
+fn p2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let mut max_joltage = 0;
+            for x in (0..12) {
+                for (cursor, a) in line.char_indices() {
+                    for b in line[cursor + 1..].chars() {
+                        let n = format!("{a}{b}").parse::<usize>().unwrap();
+                        if max_joltage < n {
+                            max_joltage = n;
+                        }
+                    }
+                }
+            }
+            max_joltage
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::p1;
+    use crate::{p1, p2};
 
     #[test]
-    fn p1_parts() {
+    fn p1_test() {
         let tests = [
             ("987654321111111", 98),
             ("811111111111119", 89),
@@ -60,5 +64,23 @@ mod tests {
         tests
             .into_iter()
             .for_each(|(input, expected)| assert_eq!(expected, p1(input)))
+    }
+
+    #[test]
+    fn p2_test() {
+        let tests = [
+            ("987654321111111", 987654321111),
+            ("811111111111119", 811111111119),
+            ("234234234234278", 434234234278),
+            ("818181911112111", 888911112111),
+            (
+                "987654321111111\n811111111111119\n234234234234278\n818181911112111",
+                3121910778619,
+            ),
+        ];
+
+        tests
+            .into_iter()
+            .for_each(|(input, expected)| assert_eq!(expected, p2(input)))
     }
 }
